8b7f462fdd8b09d985f12800f4794027617a818b
Rohith Varma Datla <r0h17h@Rohiths-MacBook-Air.local>
2025-04-24 00:09:03 +0530

    intialised clerk

--- frontend/src/app/hero-section.tsx
+++ frontend/src/app/hero-section.tsx
@@ -19,23 +19,6 @@ const HeroSection = () => {
 
       <div className="relative z-10 flex flex-col h-full">
         {/* Navigation */}
-        <nav className="flex justify-between items-center p-6 border-blue-200 bg-white bg-opacity-90">
-          <div className="font-bold text-2xl">amEx</div>
-          <div className="flex gap-6 items-center">
-            <Link href="/about" className="text-gray-800 hover:text-gray-600">
-              About
-            </Link>
-            <Link href="/login" className="text-gray-800 hover:text-gray-600">
-              Log In
-            </Link>
-            <Link 
-              href="/signup" 
-              className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800"
-            >
-              Sign Up
-            </Link>
-          </div>
-        </nav>
 
         {/* Content overlay */}
 <div className="flex flex-1 items-start justify-start p-[10px] ml-20">
